747eac6bc0e483b119c78412d5a18973f437aec6
0x0645 <mostafaelsaigh1@gmail.com>
2021-03-27 21:50:05 +0200

    test

--- project/settings.py
+++ project/settings.py
@@ -132,7 +132,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = "auth.User"
